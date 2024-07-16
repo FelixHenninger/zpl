2befc89210fea8bf3d2db394b7dbc8add7283d0f
Andreas Molzer <andreas.molzer@nebumind.com>
2024-07-16 22:58:18 +0200

    Automatic SVG scaling in all directions, buggy

--- src/svg.rs
+++ src/svg.rs
@@ -18,6 +18,7 @@ quick_error! {
 pub fn pixmap_svg(
     svg_data: String,
     pix_width: u32,
+    pix_height: u32,
 ) -> Result<::image::DynamicImage, Error> {
     let mut db = fontdb::Database::new();
     db.load_system_fonts();
@@ -25,17 +26,26 @@ pub fn pixmap_svg(
     let mut options = Options::default();
     options.fontdb = Arc::new(db);
     let rtree = Tree::from_str(&svg_data, &options).ok().unwrap();
-    let pixmap_size = rtree.size();
+    let rtree_size = rtree.size();
 
-    let mut pixmap = Pixmap::new(pix_width, pix_width).unwrap();
+    let mut pixmap = Pixmap::new(pix_width, pix_height).unwrap();
     pixmap.fill(tiny_skia::Color::from_rgba8(255, 255, 255, 255));
 
     let scale =
-        (pix_width as f32 / pixmap_size.width()).min(pix_width as f32 / pixmap_size.height());
+        (pix_width as f32 / rtree_size.width()).min(pix_height as f32 / rtree_size.height());
+
+    let xsize = rtree_size.width() * scale;
+    let ysize = rtree_size.height() * scale;
+
+    let x_off = (pix_width as f32 - xsize) / 2.0;
+    let y_off = (pix_height as f32 - ysize) / 2.0;
+    assert!(x_off >= 0.0);
+    assert!(y_off >= 0.0);
 
     resvg::render(
         &rtree,
-        tiny_skia::Transform::from_scale(scale, scale),
+        // FIXME: multiply by 4.0 makes no sense.
+        tiny_skia::Transform::from_scale(scale, scale).post_translate(x_off, 4.0 * y_off),
         &mut pixmap.as_mut(),
     );
 
