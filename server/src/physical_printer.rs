a8ead0869411170d28e6661e42413113ca266caf
Andreas Molzer <andreas.molzer@nebumind.com>
2024-08-21 22:42:10 +0200

    Implement server scaffolding of communication

--- server/src/physical_printer.rs
+++ server/src/physical_printer.rs
@@ -1,41 +1,192 @@
-use super::{Label, ShutdownToken};
+use super::ShutdownToken;
+use crate::configuration::LabelPrinter;
+use crate::job::PrintJob;
 
 use tokio::{
     sync::{mpsc, oneshot},
     task::JoinSet,
 };
 
+use zpl::{command::HostStatus, device::ZplPrinter};
+
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+
 /// A unique physical printer device.
 ///
-/// We assume to operate it as an owner, when a connection to it can be made.
+/// We assume to operate it as an owner, when a connection to it can be made. This is a
+/// pre-configured builder structure for an operating connection.
+#[derive(Clone)]
 pub struct PhysicalPrinter {
-    label: Label,
+    label: Arc<LabelPrinter>,
+    status: Arc<PrinterStatus>,
+}
+
+#[derive(Default)]
+struct PrinterStatus {
+    is_up: AtomicBool,
+}
+
+#[derive(serde::Serialize)]
+pub struct StatusInformation {
+    is_up: bool,
 }
 
 pub struct Driver {
+    message: mpsc::Sender<Task>,
+    end: Option<oneshot::Sender<ShutdownToken>>,
+}
+
+/// Counter part to driver, the physical printer side.
+pub struct Connector {
     message: mpsc::Receiver<Task>,
     end: oneshot::Receiver<ShutdownToken>,
 }
 
-pub enum Task {}
+pub enum Task {
+    Job(PrintJob),
+}
+
+struct ActiveConnection {
+    label: Arc<LabelPrinter>,
+    printer: ZplPrinter,
+    device_status: HostStatus,
+}
 
 impl PhysicalPrinter {
-    pub async fn operate(self, driver: Driver) {
-        let mut label_being_printed: JoinSet<tokio::task::JoinHandle<()>> =
+    pub fn new(label: LabelPrinter) -> Self {
+        PhysicalPrinter {
+            label: Arc::new(label),
+            status: Arc::default(),
+        }
+    }
+
+    pub fn status(&self) -> StatusInformation {
+        StatusInformation {
+            is_up: self.status.is_up.load(Ordering::Relaxed),
+        }
+    }
+
+    pub async fn drive(self, con: Connector) {
+        let mut label_being_printed: JoinSet<anyhow::Result<ActiveConnection>> =
             JoinSet::new();
-        let mut driver = driver;
+        let mut con = con;
+        let mut active: Option<ActiveConnection> = None;
 
         loop {
+            if label_being_printed.is_empty() && active.is_none() {
+                eprintln!("Connecting to printer at {}", self.label.addr);
+                let label = self.label.clone();
+                let status = self.status.clone();
+
+                label_being_printed.spawn(async move {
+                    let Ok(mut printer) =
+                        ZplPrinter::with_address(label.addr).await
+                    else {
+                        anyhow::bail!("Printer can not connect");
+                    };
+
+                    let Ok(device_status) =
+                        printer.request_device_status().await
+                    else {
+                        anyhow::bail!("Printer does not give status");
+                    };
+
+                    status
+                        .is_up
+                        .fetch_or(true, std::sync::atomic::Ordering::Relaxed);
+
+                    let device_status = device_status.clone();
+
+                    Ok(ActiveConnection {
+                        printer,
+                        device_status,
+                        label,
+                    })
+                });
+            }
+
             tokio::select!(
-                _  = label_being_printed.join_next(), if !label_being_printed.is_empty() => {
+                success = label_being_printed.join_next(), if !label_being_printed.is_empty() => {
+                    match success {
+                        Some(Ok(Ok(con))) => {
+                            active = Some(con);
+                        },
+                        Some(Ok(Err(err))) => {
+                            eprintln!("{:?}", err);
+                        }
+                        Some(Err(err)) => {
+                            eprintln!("{:?}", err);
+                        }
+                        None => unreachable!(),
+                    }
                 },
                 // Back-Pressure: only accept message while not printing. Could also do a buffer
                 // but the channel already is a buffer itself. That only makes sense if we want to
                 // do a re-ordering that the channel's sequential semantics does not permit.
-                task = driver.message.recv(), if label_being_printed.is_empty() => {
-                    todo!()
+                job = con.message.recv(), if label_being_printed.is_empty() => {
+                    let Some(Task::Job(job)) = job else { break; };
+                    let active = active.take().unwrap();
+                    label_being_printed.spawn(print_label(active, job));
                 }
             )
         }
+    }
+}
+
+async fn print_label(
+    mut con: ActiveConnection,
+    job: PrintJob,
+) -> anyhow::Result<ActiveConnection> {
+    let label = tokio::task::block_in_place(|| {
+        job.into_label(&con.label.dimensions, &con.device_status.identification)
+    });
+
+    let seq = label.print(1).await?;
+    let _ = con.printer.send(seq).await?;
+    // No change in connection state, free to reuse it.
+    Ok(con)
+}
+
+impl Driver {
+    pub fn new() -> (Self, Connector) {
+        // Aggressive bound on queue length.
+        const BOUND: usize = 8;
+
+        let (msg_send, msg_recv) = mpsc::channel(BOUND);
+        let (end_send, end_recv) = oneshot::channel();
+
+        let driver = Driver {
+            message: msg_send,
+            end: Some(end_send),
+        };
+
+        let con = Connector {
+            message: msg_recv,
+            end: end_recv,
+        };
+
+        (driver, con)
+    }
+
+    pub async fn send_job(&self, job: PrintJob) -> Result<(), &'static str> {
+        match self.message.send(Task::Job(job)).await {
+            Ok(_) => Ok(()),
+            Err(_) => Err("failed to send"),
+        }
+    }
+
+    pub fn shutdown(&mut self) {
+        if let Some(sender) = self.end.take() {
+            let _ = sender.send(ShutdownToken);
+        }
+    }
+}
+
+impl Drop for Driver {
+    fn drop(&mut self) {
+        self.shutdown();
     }
 }
