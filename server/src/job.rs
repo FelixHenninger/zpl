b1049b010fb14bb00228bb69852a680b9e49317c
Andreas Molzer <andreas.molzer@nebumind.com>
2024-08-21 23:24:08 +0200

    Implement printing of constant SVG

--- server/src/job.rs
+++ server/src/job.rs
@@ -1,5 +1,8 @@
 use serde::Deserialize;
-use zpl::{command::HostIdentification, label::Label};
+use zpl::{
+    command::HostIdentification,
+    label::{Label, LabelContent, Unit},
+};
 
 use crate::configuration::LabelDimensions;
 
@@ -11,16 +14,23 @@ impl PrintJob {
         &self,
         dim: &LabelDimensions,
         host: &HostIdentification,
+        code: String,
     ) -> Label {
-        let cwidth = ((dim.width - dim.margin_left - dim.margin_right).max(0.0)
-            * (host.dpmm as f32)) as u32;
-        let cheight = ((dim.height - dim.margin_top - dim.margin_bottom)
-            .max(0.0)
-            * (host.dpmm as f32)) as u32;
+        let cwidth = (dim.width - dim.margin_left - dim.margin_right).max(0.0);
+        let cheight =
+            (dim.height - dim.margin_top - dim.margin_bottom).max(0.0);
 
-        let width = (dim.width * (host.dpmm as f32)) as u32;
-        let height = (dim.height * (host.dpmm as f32)) as u32;
+        let width = dim.width as u32;
+        let height = dim.height as u32;
         let mut label = Label::new(width, height, host.dpmm);
+
+        label.content.push(LabelContent::Svg {
+            code,
+            x: Unit::Millimetres(dim.margin_left),
+            y: Unit::Millimetres(dim.margin_top),
+            w: Unit::Millimetres(cwidth),
+            h: Unit::Millimetres(cheight),
+        });
 
         label
     }
