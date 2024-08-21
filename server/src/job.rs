5b7d240aae2073fa7f64f6e6019c081411948fda
Aurelia Molzer <5550310+197g@users.noreply.github.com>
2024-08-22 00:16:19 +0200

    Annotate each queue task with a name in debug

--- server/src/job.rs
+++ server/src/job.rs
@@ -7,14 +7,16 @@ use zpl::{
 use crate::configuration::LabelDimensions;
 
 #[derive(Deserialize)]
-pub struct PrintJob {}
+pub enum PrintJob {
+    #[serde(rename = "svg")]
+    Svg(String),
+}
 
 impl PrintJob {
     pub fn into_label(
         &self,
         dim: &LabelDimensions,
         host: &HostIdentification,
-        code: String,
     ) -> Label {
         let cwidth = (dim.width - dim.margin_left - dim.margin_right).max(0.0);
         let cheight =
@@ -24,13 +26,17 @@ impl PrintJob {
         let height = dim.height as u32;
         let mut label = Label::new(width, height, host.dpmm);
 
-        label.content.push(LabelContent::Svg {
-            code,
-            x: Unit::Millimetres(dim.margin_left),
-            y: Unit::Millimetres(dim.margin_top),
-            w: Unit::Millimetres(cwidth),
-            h: Unit::Millimetres(cheight),
-        });
+        match self {
+            PrintJob::Svg(code) => {
+                label.content.push(LabelContent::Svg {
+                    code: code.clone(),
+                    x: Unit::Millimetres(dim.margin_left),
+                    y: Unit::Millimetres(dim.margin_top),
+                    w: Unit::Millimetres(cwidth),
+                    h: Unit::Millimetres(cheight),
+                });
+            }
+        }
 
         label
     }
