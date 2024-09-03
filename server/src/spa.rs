aac37154ebfc48874e1737ad50f89f354d1a0921
Franz Pletz <fpletz@fnordicwalking.de>
2024-09-03 18:30:12 +0200

    Overwrite default text/plain content-type header

--- server/src/spa.rs
+++ server/src/spa.rs
@@ -1,14 +1,11 @@
-use axum::{
-    http::header::CONTENT_TYPE,
-    response::{AppendHeaders, IntoResponse},
-};
+use axum::{http::header::CONTENT_TYPE, response::IntoResponse};
 
 pub async fn frontpage() -> impl IntoResponse {
     const INDEX: &str = include_str!("index.html");
-    (AppendHeaders([(CONTENT_TYPE, "text/html")]), INDEX)
+    ([(CONTENT_TYPE, "text/html")], INDEX)
 }
 
 pub async fn static_style_css() -> impl IntoResponse {
     const STYLE: &str = include_str!("style.css");
-    (AppendHeaders([(CONTENT_TYPE, "text/css")]), STYLE)
+    ([(CONTENT_TYPE, "text/css")], STYLE)
 }
