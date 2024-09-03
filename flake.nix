9ec1abe8f356c2bc9372cf322c50dcc90c0956e5
Franz Pletz <fpletz@fnordicwalking.de>
2024-09-03 16:10:18 +0200

    flake: Refactor to use flake-parts, build packages

--- flake.nix
+++ flake.nix
@@ -1,61 +1,105 @@
 {
-  description = "Glia dev environment";
+  description = "ZPL dev environment";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    crane.url = "github:ipetkov/crane";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        customRust = pkgs.rust-bin.stable.latest.default.override {
-          # None at present
-        };
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          nativeBuildInputs = [
-          ] ++ (
-            # Load alsa dependencies on Linux
-            lib.optionals stdenv.isLinux [
-              pkg-config
-              alsa-oss
-              alsa-lib
-            ]
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      imports = [ inputs.treefmt-nix.flakeModule ];
+
+      perSystem =
+        {
+          pkgs,
+          system,
+          config,
+          lib,
+          ...
+        }:
+        let
+          craneLib = inputs.crane.mkLib pkgs;
+
+          src =
+            let
+              spaFilter = path: _type: builtins.match ".*(html|css)$" path != null;
+              filter = path: type: (spaFilter path type) || (craneLib.filterCargoSources path type);
+            in
+            lib.cleanSourceWith {
+              src = ./.;
+              inherit filter;
+              name = "source";
+            };
+
+          commonArgs = {
+            inherit src;
+            strictDeps = true;
+            buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.libiconv ];
+          };
+
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+          individualCrateArgs = commonArgs // {
+            inherit cargoArtifacts;
+          };
+
+          zpl = craneLib.buildPackage individualCrateArgs;
+
+          server = craneLib.buildPackage (
+            individualCrateArgs
+            // {
+              inherit (craneLib.crateNameFromCargoToml { src = ./server; }) pname version;
+              cargoExtraArgs = "-p zpl-server";
+            }
           );
+        in
+        {
+          _module.args.pkgs =
+            inputs.nixpkgs.legacyPackages.${system}.extend
+              inputs.rust-overlay.overlays.default;
 
-          buildInputs = [
-            customRust
-            bacon
-            python3
-            python3Packages.pillow
-            python3Packages.crc
-            mob
-          ] ++ (
-            # Apple libraries if necessary
-            lib.optional stdenv.isDarwin [
-              libiconv
-              darwin.apple_sdk.frameworks.AppKit
-              darwin.apple_sdk.frameworks.AudioToolbox
-              darwin.apple_sdk.frameworks.AudioUnit
-              darwin.apple_sdk.frameworks.CoreAudio
-              darwin.apple_sdk.frameworks.CoreFoundation
-            ]
-          );
+          packages = {
+            default = zpl;
+            inherit server;
+          };
 
-          shellHook = ''
-            export RUST_LOG=debug
-          '';
+          devShells.default = craneLib.devShell {
+            packages = [ config.treefmt.build.wrapper ];
+            RUST_LOG = "debug";
+          };
+
+          treefmt = {
+            projectRootFile = "flake.lock";
+
+            settings.formatter = {
+              nix = {
+                command = pkgs.nixfmt-rfc-style;
+                includes = [ "*.nix" ];
+              };
+              rustfmt = {
+                command = pkgs.rustfmt;
+                options = [
+                  "--edition"
+                  "2021"
+                ];
+                includes = [ "*.rs" ];
+              };
+            };
+          };
         };
-
-        formatter = pkgs.nixpkgs-fmt;
-      }
-    );
+    };
 }
